ba134a112a2fb359937d22b0416dbeef32634873
Lothar Rubusch <l.rubusch@gmail.com>
2021-06-23 23:20:45 +0200

    yocto: initial setup (untested)
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- yocto/build_context/build.sh
+++ yocto/build_context/build.sh
@@ -3,17 +3,20 @@
 # references:
 # https://github.com/sifive/meta-sifive
 
-export USER="$(whoami)"
+export MY_USER="$(whoami)"
 export YOCTO_BRANCH="dunfell"
-export MY_HOME="/home/${USER}"
-export YOCTO_DIR="${MY_HOME}/poky"
-export BUILDDIR="${YOCTO_DIR}/build"
+export MY_HOME="/home/${MY_USER}"
+export YOCTO_DIR="${MY_HOME}/poky-${YOCTO_BRANCH}"
+export BUILD_DIR="${YOCTO_DIR}/build"
 
 
 ## this may take time
-# TODO check with stat   
-sudo chown ${USER}:${USER} -R ${YOCTO_DIR}
-
+for item in ${BUILD_DIR} ${YOCTO_DIR}; do
+    if [ ! "${MY_USER}" == "$( stat -c %U ${item} )" ]; then
+        ## may take some time
+        sudo chown ${MY_USER}:${MY_USER} -R ${item}
+    fi
+done
 
 cd ${YOCTO_DIR}
 
@@ -33,12 +36,13 @@ chmod a+x ./meta-sifive/setup.sh
 source ./openembedded-core/oe-init-build-env
 
 ## parallelize build
+## NB: check out meta-sifi and its readme for different images, settings, toolchain, etc.
 export PARALLEL_MAKE="-j 4"
 export BB_NUMBER_THREADS=4
-export MACHINE="freedom-u540"
+export MACHINE="freedom-u740"
 
 ## build
-bitbake demo-coreip-cli
+bitbake demo-coreip-cli || exit 1
 
 echo "READY."
 echo
