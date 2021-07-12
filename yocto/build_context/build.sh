c003e55d39cbeec5720f48fc4dc4eab8d77b43f3
Lothar Rubusch <l.rubusch@gmail.com>
2021-07-12 23:18:44 +0200

    build.sh: shell checked
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- yocto/build_context/build.sh
+++ yocto/build_context/build.sh
@@ -1,10 +1,8 @@
-#!/bin/bash -e
+#!/bin/sh -ex
 #
 # references:
 # https://github.com/sifive/meta-sifive
-set -x          
 MY_USER="$(whoami)"
-YOCTO_BRANCH="dunfell"
 MY_HOME="/home/${MY_USER}"
 YOCTO_DIR="${MY_HOME}/poky"
 BUILD_DIR="${YOCTO_DIR}/build"
@@ -12,15 +10,15 @@ BUILD_DIR="${YOCTO_DIR}/build"
 ## permissions
 for item in "${YOCTO_DIR}" "${BUILD_DIR}" "${MY_HOME}/.gitconfig" "${MY_HOME}/.ssh"; do
     test -e "${item}" || continue
-    if [ ! "${MY_USER}" == "$( stat -c %U ${item} )" ]; then
+    if [ ! "${MY_USER}" = "$( stat -c %U "${item}" )" ]; then
         ## may take some time
-        sudo chown ${MY_USER}:${MY_USER} -R ${item}
+        sudo chown "${MY_USER}:${MY_USER}" -R "${item}"
     fi
 done
 
 ## sources
-cd ${YOCTO_DIR}
-if [[ ! -d .repo ]]; then
+cd "${YOCTO_DIR}"
+if [ ! -d .repo ]; then
     repo init -u git://github.com/sifive/meta-sifive -b 2021.05 -m tools/manifests/sifive.xml
     repo sync
     repo start work --all
@@ -32,7 +30,7 @@ chmod a+x ./meta-sifive/setup.sh
 
 
 ## enter environment
-source ./openembedded-core/oe-init-build-env
+. ./openembedded-core/oe-init-build-env
 
 ## parallelize build
 ## NB: check out meta-sifi and its readme for different images, settings, toolchain, etc.
