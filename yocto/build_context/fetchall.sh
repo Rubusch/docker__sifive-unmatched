59dd831f3a5a925297e016914acc88812ba724eb
Lothar Rubusch <l.rubusch@gmail.com>
2021-07-14 23:51:42 +0200

    build.sh: in a fight with ci, separate full fetch all and build

---
+++ yocto/build_context/fetchall.sh
@@ -0,0 +1,58 @@
+#!/bin/sh -ex
+#
+# references:
+# https://github.com/sifive/freedom-u-sdk
+
+MY_USER="$(whoami)"
+MY_HOME="/home/${MY_USER}"
+SSH_DIR="${MY_HOME}/.ssh"
+SSH_KNOWN_HOSTS="${SSH_DIR}/known_hosts"
+REPO_BRANCH="2021.06"
+YOCTO_DIR="${MY_HOME}/poky"
+BUILD_DIR="${YOCTO_DIR}/build"
+
+## permissions
+for item in "${YOCTO_DIR}" "${BUILD_DIR}" "${MY_HOME}/.gitconfig" "${MY_HOME}/.ssh"; do
+    test -e "${item}" || continue
+    if [ ! "${MY_USER}" = "$( stat -c %U "${item}" )" ]; then
+        ## may take some time
+        sudo chown "${MY_USER}:${MY_USER}" -R "${item}"
+    fi
+done
+
+## ssh known_hosts
+touch "${SSH_KNOWN_HOSTS}"
+for item in "github.com" "bitbucket.org"; do
+    if [ "" = "$( grep "${item}" -r "${SSH_KNOWN_HOSTS}" )" ]; then
+        ssh-keyscan "${item}" >> "${SSH_KNOWN_HOSTS}"
+    fi
+done
+
+## sources
+if [ ! -d "${YOCTO_DIR}/.repo" ]; then
+    cd "${YOCTO_DIR}"
+    repo init -u git://github.com/sifive/meta-sifive -b "${REPO_BRANCH}" -m tools/manifests/sifive.xml
+    repo sync
+    repo start work --all
+fi
+
+## final installation
+cd "${YOCTO_DIR}"
+chmod a+x ./meta-sifive/setup.sh
+./meta-sifive/setup.sh
+
+## enter environment
+cd "${YOCTO_DIR}/openembedded-core"
+. ./oe-init-build-env build
+
+## parallelize build
+## NB: check out meta-sifi and its readme for different images, settings, toolchain, etc.
+export PARALLEL_MAKE="-j 4"
+export BB_NUMBER_THREADS=4
+export MACHINE="unmatched"
+
+## build
+bitbake demo-coreip-cli --forall=fetch || exit 1
+
+echo "READY."
+echo
