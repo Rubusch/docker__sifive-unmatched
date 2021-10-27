3fb1ea639ca9b61dd013025affef53ff4c2c6c87
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-27 21:25:01 +0200

    docker-setup: cleanup
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-## sandbox (base image)
+## base image
 IMAGE="sandbox"
 
 TAG="$(grep "ENV DOCKER_BASE_TAG" -r ./docker/build_context/Dockerfile | awk -F= '{ print $2 }')"
@@ -9,8 +9,8 @@ TAG="${TAG//\"}"
 CONTAINER="$(docker images | grep "/${IMAGE}" | grep "${TAG}" | awk '{print $3}')"
 
 if [ -z "${CONTAINER}" ]; then
-	git clone https://github.com/Rubusch/docker__sandbox.git sandbox
-	pushd ./sandbox &> /dev/null
+	git clone "https://github.com/Rubusch/docker__${IMAGE}.git" "${IMAGE}"
+	pushd ./"${IMAGE}" &> /dev/null
 	echo "UID=$(id -u)" > .env
 	echo "GID=$(id -g)" >> .env
 	docker-compose up
